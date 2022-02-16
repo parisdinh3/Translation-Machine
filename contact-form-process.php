303d12ea06c514143e33d54ee847cbcfb85c9e14
parisdinh3 <66796491+parisdinh3@users.noreply.github.com>
2022-02-16 10:47:41 -0500

    d

---
+++ contact-form-process.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("parisldinh@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: parisldinh@gmail.com");
+
+
+}
+
+
+?>
