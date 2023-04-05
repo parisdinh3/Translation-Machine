934896f2c21116e37af5c11fc7f232983ccd8828
parisdinh3 <66796491+parisdinh3@users.noreply.github.com>
2023-04-04 22:38:55 -0400

    s

--- functions.js
+++ functions.js
@@ -2,3 +2,56 @@
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+Particles.
+init
+({
+
+// normal options
+  selector:
+'.background'
+,
+  maxParticles:
+450
+,
+
+// options for breakpoints
+  responsive: [
+    {
+      breakpoint:
+768
+,
+      options: {
+        maxParticles:
+200
+,
+        color:
+'#48F2E3'
+,
+        connectParticles:
+false
+      }
+    }, {
+      breakpoint:
+425
+,
+      options: {
+        maxParticles:
+100
+,
+        connectParticles:
+true
+      }
+    }, {
+      breakpoint:
+320
+,
+      options: {
+        maxParticles:
+0
+
+// disables particles.js
+      }
+    }
+  ]
+});
